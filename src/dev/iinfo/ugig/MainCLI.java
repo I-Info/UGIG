94b2608c003282ba21dc2acea8becdcff098fe4d
I_Info <49438478+I-Info@users.noreply.github.com>
2021-06-05 11:53:21 +0800

    Solve conflicts

--- src/dev/iinfo/ugig/MainCLI.java
+++ src/dev/iinfo/ugig/MainCLI.java
@@ -25,27 +25,6 @@ public class MainCLI {
                 System.out.println("Invalid input");
                 continue;
             }
-
-    private static final int DEFAULT_WINDOW_WIDTH = 400;
-    private static final int DEFAULT_WINDOW_HEIGHT = 300;
-
-    public static void main(String[] args) {
-        JFrame mainWindow = new JFrame();
-        mainWindow.setLayout(null);
-        // Some default settings
-        mainWindow.setTitle("UGIG-Game");
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = screenSize.width / 2 - DEFAULT_WINDOW_WIDTH / 2;
-        int y = screenSize.height / 2 - DEFAULT_WINDOW_HEIGHT / 2;
-        mainWindow.setBounds(x, y, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
-        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainWindow.setResizable(false);
-
-        JLabel jLabel1 = new JLabel("Hello", JLabel.CENTER);
-        jLabel1.setBounds(150, 1, 100, 20);
-        jLabel1.setFont(new Font("", Font.BOLD, 20));
-        mainWindow.add(jLabel1);
-
-        mainWindow.setVisible(true);
+        }
     }
 }
